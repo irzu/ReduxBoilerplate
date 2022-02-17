2f82720cafdd889e6dce1d1a03c5573bdbe030b9
Irek Żuchowski <irek.zuchowski@volue.com>
2022-02-17 12:18:17 +0100

    sorted imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import './App.css';
 import { HelloWorld } from "./features/HelloWorld/components/HelloWorld/HelloWorld";
+import './App.css';
 
 const App = () => (
     <>
