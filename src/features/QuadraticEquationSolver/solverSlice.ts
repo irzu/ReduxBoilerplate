9c5e76ea6a917c6558b11737fbb37941f68bab4f
Irek Żuchowski <irek.zuchowski@volue.com>
2022-02-17 21:30:44 +0100

    add typings

--- src/features/QuadraticEquationSolver/solverSlice.ts
+++ src/features/QuadraticEquationSolver/solverSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { EquationParams, SolverState } from "./types";
-import { getSolutionAsync, solveEquation } from './common/utils/solutionProcessor';
+import { Solution, SolverState } from "./types";
+import { getSolutionAsync } from './common/utils/solutionProcessor';
 
 const initialState: SolverState = {
     solutions: [],
@@ -11,28 +11,21 @@ const initialState: SolverState = {
 const solverSlice = createSlice({
     name: 'solver',
     initialState,
-    reducers: {
-        getSolution(state, action:PayloadAction<EquationParams>) {
-            const { a, b, c } = action.payload;
-            const result = solveEquation(a, b, c);
-            state.solutions.push({...result, a, b, c});
-        },
-    },
+    reducers: {},
     extraReducers: {
         [getSolutionAsync.pending.type]: (state) => {
             state.isProcessing = true;
             state.processingError = '';
         },
-        [getSolutionAsync.fulfilled.type]: (state, action) => {
+        [getSolutionAsync.fulfilled.type]: (state, action: PayloadAction<Solution>) => {
             state.isProcessing = false;
             state.solutions.push(action.payload);
         },
-        [getSolutionAsync.rejected.type]: (state, action) => {
+        [getSolutionAsync.rejected.type]: (state, action: PayloadAction<string>) => {
             state.isProcessing = false;
             state.processingError = action.payload;
         }
     }
 });
 
-export const { getSolution } = solverSlice.actions;
 export default solverSlice.reducer;
