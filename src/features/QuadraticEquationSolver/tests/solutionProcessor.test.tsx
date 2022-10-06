398e44c30cbfee6b182c484f233afbd12b8bd5bc
Irek Zuchowski <irek.zuchowski_ext@scout24.com>
2022-10-06 10:47:54 +0200

    async tests

--- src/features/QuadraticEquationSolver/tests/solutionProcessor.test.tsx
+++ src/features/QuadraticEquationSolver/tests/solutionProcessor.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { createTestStore } from '../../../common/utils/testUtils';
 import { getSolutionAsync } from '../solverAPI';
@@ -19,7 +19,7 @@ describe("Test Quadratic Equation solver App", () => {
                 <SolutionDisplay />
             </Provider>
         );
-        expect(getByTestId('solution')).toHaveTextContent('The equation 4x2+ 4x + 1 = 0 has one root: -0.5');
+        await waitFor(() =>expect(getByTestId('solution')).toHaveTextContent('The equation 4x2+ 4x + 1 = 0 has one root: -0.5'));
     });
 
     it('solves equation that has two real roots', async() => {
@@ -29,7 +29,7 @@ describe("Test Quadratic Equation solver App", () => {
                 <SolutionDisplay />
             </Provider>
         );
-        expect(getByTestId('solution')).toHaveTextContent('The equation 10x2+ 5x + 0 = 0 has two roots: 0 and -0.5');
+        await waitFor(() =>expect(getByTestId('solution')).toHaveTextContent('The equation 10x2+ 5x + 0 = 0 has two roots: 0 and -0.5'));
     });
 
     it('solves equation that has two complex roots', async() => {
@@ -39,6 +39,6 @@ describe("Test Quadratic Equation solver App", () => {
                 <SolutionDisplay />
             </Provider>
         );
-        expect(getByTestId('solution')).toHaveTextContent('The equation 10x2+ 5x + 34 = 0 has two roots: -0.25 + 1.83i and -0.25 - 1.83i');
+        await waitFor(() =>expect(getByTestId('solution')).toHaveTextContent('The equation 10x2+ 5x + 34 = 0 has two roots: -0.25 + 1.83i and -0.25 - 1.83i'));
     });
 });
