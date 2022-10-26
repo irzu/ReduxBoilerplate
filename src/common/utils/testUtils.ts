f80840dcc74b255dd891125b7060fcf4b6703311
Irek Zuchowski <irek.zuchowski_ext@scout24.com>
2022-10-26 09:17:24 +0200

    lint

--- src/common/utils/testUtils.ts
+++ src/common/utils/testUtils.ts
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import helloWorldReducer from '../../features/HelloWorld/helloWorldSlice';
 import solverReducer from '../../features/QuadraticEquationSolver/solverSlice';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
 
 export const createTestStore = () => (
     createStore(
