e6d9d65045ae2fc67fb4ca9ac19d2737e013c1c4
irzu <irek@atarnia.com>
2025-04-16 17:57:59 +0200

    store cleanup

--- src/store.ts
+++ src/store.ts
@@ -2,15 +2,9 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import helloWorldReducer from './features/HelloWorld/helloWorldSlice';
-// import solverReducer from './features/QuadraticEquationSolver/solverSlice';
-// import countriesReducer from './features/Countries/countriesSlice';
-// import provincesReducer from './features/Provinces/provincesSlice';
 
 const reducer = {
     helloWorld: helloWorldReducer,
-    // solver: solverReducer,
-    // countries: countriesReducer,
-    // provinces: provincesReducer
 };
 
 const devTools = process.env.NODE_ENV === "development" ? [logger]: [];
