c9c09fad06a74eeafefee0b58a3c6dbe89361e81
irzu <irek@atarnia.com>
2025-04-21 20:07:43 +0200

    welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,19 @@ const App = () => {
         <>
             <header>Redux Toolkit Playground</header>
             <div className="main">
-                <HelloWorld/>
-                <SolverApp />
-                <SolverAppAsync />
-                <CountriesList />
+                <h3>Welcome to Redux Toolkit Playground</h3>
+                <p>Go to App.tsx and uncomment the apps you want to play with, altenatively follow the tutorial step by step and see the codebase incrementing gradually by switching to one of below branches:</p>
+                <ul>
+                    <li>chapter1-HelloWorld</li>
+                    <li>chapter2-EquationSolver</li>
+                    <li>chapter3-EquationSolverAsync</li>
+                    <li>chapter4-CountriesList</li>
+                </ul>
+                <hr/>
+                {/* <HelloWorld/> */}
+                {/* <SolverApp /> */}
+                {/* <SolverAppAsync /> */}
+                {/* <CountriesList /> */}
             </div>
         </>
     );
